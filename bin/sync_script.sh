76df64588c954ac197037cc9d0217834cd3e3436
IA (naruto522ru) <itachi522ru@gmail.com>
2024-11-06 03:54:48 +0300

    Add if in script

--- bin/sync_script.sh
+++ bin/sync_script.sh
@@ -103,4 +103,8 @@ if [[ $(curl -is -H "$USER_AGENT" -k -4s --max-time 90 --retry-delay 3 --retry 5
 else
     echo "$(date +"%d.%m.%y %H:%M:%S"): Скрипт ${NAME_SCRIPT} недоступен. Не скачиваем его на GitHub,оставляем старый\!"
 fi
+
+# Условие проверяет не пусты ли сами файлы. Дело в том, что файл может существовать на удаленном сервере, но может быть пустым от такого первое if не спасает. Если такой будет, то делаем git restore , чтобы не закоммить в git пустой файл.
+for filename in {tracks,online_mod.js,e.js,o.js,rating.js,tmdb-proxy,ts-preload.js,tricks.js,tv.js}; do if [[ -s ${PATH_DOWNLOAD}${filename} ]]; then echo "No empty $filename";else echo -e "\e[1;33mEmpty\033[0m $filename" && git restore ./$filename; fi; done
+
 exit 0
